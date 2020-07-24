b1a037204a91f8c040e738d36ee9e0ec9da1c40f
Dziemo <michalek210@gmail.com>
2020-07-25 00:03:49 +0200

    Multiline check working

---
+++ JoesTatooStudio/Assets/PathDisplayer.cs
@@ -0,0 +1,19 @@
+﻿using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDisplayer : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    PathCreator pathCreator;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        pathCreator = GetComponent<PathCreator>();
+
+        lineRenderer.positionCount = pathCreator.path.NumPoints;
+        lineRenderer.SetPositions(pathCreator.path.localPoints);
+    }
+}
